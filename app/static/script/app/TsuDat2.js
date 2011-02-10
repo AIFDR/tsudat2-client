83a397a251457cef20f371dce2e76fb9cde0836e
ahocevar <andreas.hocevar@gmail.com>
2011-02-10 17:52:21 +0100

    basic application layout with generic tools

---
+++ app/static/script/app/TsuDat2.js
@@ -0,0 +1,134 @@
+Ext.BLANK_IMAGE_URL = "theme/app/img/blank.gif";
+OpenLayers.DOTS_PER_INCH = 25.4 / 0.28;
+OpenLayers.ImgPath = "externals/openlayers/img/";
+OpenLayers.Layer.WMS.prototype.DEFAULT_PARAMS.transparent = true;
+OpenLayers.Layer.Google.v3.animationEnabled = false;
+
+var TsuDat2 = Ext.extend(gxp.Viewer, {
+    
+    /** i18n:
+    layersTabTitle: "Layers",
+    legendTabTitle: "Legend",
+    step1Title: "Step 1. Tsunami Scenario",
+    step2Title: "Step 2. Tsunami Simulation Area",
+    step3Title: "Step 3. Tsunami Source",
+    step4Title: "Step 4. Generate Tsunami Simulation"
+    */
+
+    defaultSourceType: "gxp_wmssource",
+
+    constructor: function(config) {
+                
+        Ext.applyIf(config.map, {
+            id: "map",
+            region: "center",
+            controls: [
+                new OpenLayers.Control.Navigation({zoomWheelOptions: {interval: 250}}),
+                new OpenLayers.Control.PanPanel({slideRatio: .5}),
+                new OpenLayers.Control.ZoomPanel(),
+                new OpenLayers.Control.Attribution()
+            ]
+        });
+
+        config.mapItems = [{
+            xtype: "gx_zoomslider",
+            vertical: true,
+            height: 100
+        }];
+        
+        config.portalItems = [{
+            region: "center",
+            layout: "border",
+            tbar: {
+                id: "paneltbar",
+                items: [{
+                    iconCls: "icon-geoexplorer",
+                    disabled: true
+                }, "TsuDat2", "-", "-"]
+            },
+            items: [{
+                xtype: "tabpanel",
+                id: "west",
+                region: "west",
+                width: 200,
+                split: true,
+                collapsible: true,
+                collapsed: true,
+                collapseMode: "mini",
+                header: false,
+                listeners: {
+                    "add": {
+                        fn: function(cmp) { cmp.setActiveTab(0); },
+                        single: true
+                    }
+                }
+            }, {
+                id: "east",
+                region: "east",
+                layout: "accordion",
+                width: 300,
+                split: true,
+                collapsible: true,
+                collapseMode: "mini",
+                header: false,
+                border: false,
+                items: [{
+                    title: this.step1Title
+                }, {
+                    title: this.step2Title
+                }, {
+                    title: this.step3Title
+                }, {
+                    title: this.step4Title
+                }]
+            },
+            "map"]
+        }];
+        
+        config.tools = [{
+            ptype: "gxp_zoomtoextent",
+            extent: config.map.extent,
+            closest: false,
+            actionTarget: {target: "paneltbar", index: 3}
+        }, {
+            ptype: "gxp_navigationhistory",
+            actionTarget: {target: "paneltbar", index: 4}
+        }, {
+            ptype: "gxp_measure",
+            actionTarget: {target: "paneltbar", index: 6},
+            toggleGroup: "main"
+        }, {
+            ptype: "gxp_wmsgetfeatureinfo",
+            actionTarget: {target: "paneltbar", index: 7},
+            toggleGroup: "main"
+        }, {
+            ptype: "gxp_layertree",
+            outputTarget: "west",
+            outputConfig: {
+                title: this.layersTabTitle,
+                id: "tree",
+                tbar: []
+            }
+        }, {
+            ptype: "gxp_addlayers",
+            actionTarget: "tree.tbar"
+        }, {
+            ptype: "gxp_removelayer",
+            actionTarget: ["tree.tbar", "tree.contextMenu"]
+        }, {
+            ptype: "gxp_zoomtolayerextent",
+            closest: false,
+            actionTarget: ["tree.tbar", "tree.contextMenu"]
+        }, {
+            outputTarget: "west",
+            outputConfig: {
+                xtype: "gx_legendpanel",
+                title: this.legendTabTitle,
+                padding: 5
+            }
+        }]
+
+        TsuDat2.superclass.constructor.apply(this, arguments);
+    }
+
+});
---
+++ app/static/script/app/locale/en.js
@@ -0,0 +1,12 @@
+GeoExt.Lang.add("en", {
+
+    "TsuDat2.prototype": {
+        layersTabTitle: "Layers",
+        legendTabTitle: "Legend",
+        step1Title: "Step 1. Tsunami Scenario",
+        step2Title: "Step 2. Tsunami Simulation Area",
+        step3Title: "Step 3. Tsunami Source",
+        step4Title: "Step 4. Generate Tsunami Simulation"
+    }
+
+});
