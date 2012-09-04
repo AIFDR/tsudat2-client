4338161e9f07fa4e7932a07dcc87a81b7389ef38
Bart van den Eijnden <bartvde@opengeo.org>
2012-09-04 13:37:44 +0200

    latest buildkit

--- app/autoloader.js
+++ app/autoloader.js
@@ -1,47 +1,20 @@
 // the autoloader injects scripts into the document dynamically
 // only suitable for development/debug mode
 
+var {Application} = require("stick");
 var FS = require("fs");
-var STATIC = require("ringo/middleware/static").middleware;
+var CONFIG = require("buildkit").config;
+var MERGE = require("buildkit").merge;
 
-// TODO: unhack this
-var path = FS.normal(FS.join(module.directory, "..", "build", "buildkit", "lib"));
-require.paths.push(path);
-var CONFIG = require("buildkit/config");
-var MERGE = require("buildkit/merge");
+var template = getResource("./templates/debug-loader.js").getContent();
 
-// TODO: convert this to a real template
-var template = '                                                            \n\
-(function() {                                                               \n\
-                                                                            \n\
-    var jsfiles = [@paths@];                                                \n\
-                                                                            \n\
-    var scripts = document.getElementsByTagName("script");                  \n\
-    var parts = scripts[scripts.length-1].src.split("/");                   \n\
-    parts.pop();                                                            \n\
-    var path = parts.join("/");                                             \n\
-                                                                            \n\
-    var pieces = new Array(jsfiles.length);                                 \n\
-                                                                            \n\
-    var element = document.getElementsByTagName("head").length ?            \n\
-                    document.getElementsByTagName("head")[0] :              \n\
-                    document.body;                                          \n\
-    var script, src;                                                        \n\
-                                                                            \n\
-    for(var i=0; i<jsfiles.length; i++) {                                   \n\
-        src = path + "/" + jsfiles[i];                                      \n\
-        pieces[i] = "<script src=\'" + src + "\'></script>";            \n\
-    }                                                                       \n\
-    document.write(pieces.join(""));                                    \n\
-})();                                                                       \n\
-';
-
-var libLoader = function(section, order) {
-    var paths = [];
-    order.forEach(function(path) {
-        paths.push("'@" + section + "/" + path + "'");
+var libLoader = function(section, order, baseDir) {
+    var paths = order.map(function(script) {
+        // TODO: resolve path issues elsewhere
+        var relative = FS.relative(FS.join(baseDir, "."), script.root).replace(/\\/g, "/");
+        return "'@/" + relative + "/" + script.path + "'";
     });
-    var body = template.replace("@paths@", paths.join(",\n"));    
+    var body = template.replace("{{paths}}", paths.join(",\n"));
     return function(env) {
         return {
             status: 200,
@@ -51,96 +24,29 @@ var libLoader = function(section, order) {
     };
 };
 
-var scriptLoader = function(root, script) {
-    return function(env) {
-        var path = FS.join(root, script);
-        var body = FS.read(path);
-        return {
-            status : 200,
-            headers : {
-                "Last-Modified": FS.lastModified(path).toUTCString(),
-                "Content-Type": "text/javascript",
-                "Content-Length": body.length.toString(10)
-            },
-            body : [body]
-        };
-    };
-};
-
-
-var notFound = function() {
-    throw {notfound: true};
-};
-
 var App = function(config) {
-    
     var sections = CONFIG.parse(config);
-    var group, root, order, urls = {};
+    var group, root, order;
+    var app = Application();
+    app.configure("mount");
+    var configDir = FS.directory(config);
     for (var section in sections) {
         group = sections[section];
-        // make root relative to config
-        root = FS.join(FS.directory(config), group.root[0]);
-        group.root = [root];
+        // absolute path for group roots
+        // TODO: resolve path issues elsewhere
+        group.root = group.root.map(function(path) {
+            return FS.join(configDir, path);
+        });
         order = MERGE.order(group);
-        // create lib loader
-        urls["/" + section] = libLoader(section, order);
-        // create static loader for all scripts in lib
-        var app = STATIC(root)(notFound);
-        urls["/@" + section] = app; 
+        // mount lib loader for the section
+        app.mount("/" + section, libLoader(section, order, configDir));
     }
-    return URLMap(urls);
-    
+    // mount a script loader for all scripts
+    var scriptLoader = Application();
+    scriptLoader.configure("static");
+    scriptLoader.static(configDir);
+    app.mount("/@", scriptLoader);
+    return app;
 };
 
 exports.App = App;
-
-var URLMap = function(map, options) {
-    var options = options || { longestMatchFirst : true },
-        mapping = [];
-        
-    for (var location in map) {
-        var app = map[location],
-            host = null,
-            match;
-        
-        if (match = location.match(/^https?:\/\/(.*?)(\/.*)/)) {
-            host = match[1];
-            location = match[2];
-        }
-            
-        if (location.charAt(0) != "/") {
-            throw new Error("paths need to start with / (was: " + location + ")");
-        }
-        
-        mapping.push([host, location.replace(/\/+$/,""), app]);
-    }
-    // if we want to match longest matches first, then sort
-    if (options.longestMatchFirst) {
-        mapping = mapping.sort(function(a, b) {
-            return (b[1].length - a[1].length) || ((b[0]||"").length - (a[0]||"").length);
-        });
-    }
-    
-    return function(env, path) {
-        path = (path || env.pathInfo).replace(/\/+$/,"");
-        var hHost = env.host, sPort = env.port;
-
-        for (var i = 0; i < mapping.length; i++) {
-            var host = mapping[i][0], location = mapping[i][1], app = mapping[i][2];
-
-            if ((host === hHost || (host === null)) &&
-                (location === path.substring(0, location.length)) &&
-                (path.charAt(location.length) === "" || path.charAt(location.length) === "/"))
-            {
-                env = Object.create(env); // make a shallow "copy", since we're modifying SCRIPT_NAME / PATH_INFO
-
-                env.scriptName += location;
-                env.pathInfo = path.substring(location.length);
-
-                return app(env);
-            }
-        }
-
-        throw {notfound: true};
-    }
-}
--- app/main.js
+++ app/main.js
@@ -1,8 +1,32 @@
-#!/usr/bin/env ringo
+var fs = require("fs");
+var {Application} = require("stick");
+
+var app = Application();
+app.configure("notfound", "error", "static", "params", "mount");
+
+var appPath = module.resolve("static");
+app.static(appPath, "index.html");
+app.mount("/proxy", require("./root/proxy").app);
+
+// debug mode loads unminified scripts
+// assumes markup pulls in scripts under the path /servlet_name/lib/
+var config = fs.normal(fs.join(module.directory, "..", "buildjs.cfg"));
+app.mount("/script/", require("./autoloader").App(config));
+
+// proxy a remote geoserver on /geoserver by setting app.proxy.geoserver to remote URL
+// only recommended for debug mode
+var geoserver = java.lang.System.getProperty("app.proxy.geoserver");
+if (geoserver) {
+    if (geoserver.charAt(geoserver.length-1) !== "/") {
+        geoserver = geoserver + "/";
+    }
+    // debug specific proxy
+    app.mount("/geoserver/", require("./root/proxy").pass({url: geoserver, preserveHost: true, allowAuth: true}));
+}
+
+exports.app = app;
 
 // main script to start application
-// when run from the command line, assume debug mode (scripts not compressed)
-if (require.main == module) {
-    java.lang.System.setProperty("app.debug", 1);
-    require("ringo/webapp").main(module.directory);
+if (require.main === module) {
+    require("ringo/httpserver").main(module.id);
 }
